bd3b8a0a39490c3efc829be7fdda6ddd80000937
RaulSaldanaIII <756853@student.dusd.net>
2025-11-19 13:18:26 -0800

    looping
    completed

--- P6SaldanaScriptingProject/Assets/Scripts/LoopingScripts/DoWhileLoops.cs
+++ P6SaldanaScriptingProject/Assets/Scripts/LoopingScripts/DoWhileLoops.cs
@@ -7,7 +7,11 @@ public class DoWhileLoops : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        bool shouldContinue = false;
+        do
+        {
+            print("Hello World");
+        } while (shouldContinue == true);
     }
 
     // Update is called once per frame
